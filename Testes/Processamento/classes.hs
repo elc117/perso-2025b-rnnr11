cdc9920f52c523d6d3fd201c935e5a5276a6d304
rnnr11 <joaopedroquadrosmartins@gmail.com>
2025-09-21 15:14:00 -0300

    Função de cálculo de mediana. Essa foi chata de deixar legível

--- Testes/Processamento/classes.hs
+++ Testes/Processamento/classes.hs
@@ -4,7 +4,12 @@ import Data.List
 -- Para facilitar meu trabalho e registrar aqui, vou lançar abaixo a definição da lista que eu vou usar para testar as funções no GHCI
 -- lst = [31, 41, 47, 53, 57, 61, 65, 67, 73, 35, 41, 48, 54, 59, 64, 66, 68, 73, 35, 42, 50, 55, 60, 65, 66, 69, 74]
 -- OBS: Já estava fazendo isso, mas só agora percebi que eu deveria colocar ao menos uma nota sobre
+-- Esse é o comando que vou usar para testar as funções de cálculo que precisam da tabela pronta
+-- table = [(31.0,39.0,3),(39.0,47.0,3),(47.0,55.0,5),(55.0,63.0,5),(63.0,71.0,8),(71.0,79.0,3)]
+-- ou
+-- table = [(72, 86, 6), (86, 100, 6), (100, 114, 10), (114, 128, 5), (128, 142, 2), (142, 156, 1)]
 
+-- Funções Auxiliares --
 -- Ordena uma lista de dados
 -- Passei muito tempo nessa, mas esqueci de salvar as versões falhas
 lstMenores :: [Float] -> Float -> [Float] -- Retorna uma lista com todos os elementos menores que o pivo
@@ -17,10 +22,6 @@ quickSort :: [Float] -> [Float] -- Ordena o vetor de floats através do quick so
 quickSort [] = []
 quickSort (pivo:lst) = quickSort (lstMenores lst pivo) ++ [pivo] ++ quickSort (lstMaiores lst pivo)
 
--- Calcular a média
-mediaData:: [Float] -> Float
-mediaData dataLst = (sum dataLst)/(fromIntegral $ length dataLst)
-
 -- Quick sort para uma lista de listas com tamanho das sublistas como parâmetro
 lstlstMenores :: [[Float]] -> [Float] -> [[Float]]
 lstlstMenores lstLst pivo = [x | x <- lstLst, length x < length pivo]
@@ -32,23 +33,55 @@ quickSortLst :: [[Float]] -> [[Float]]
 quickSortLst [] = []
 quickSortLst (pivo:lstLst) = quickSortLst (lstlstMaiores lstLst pivo) ++ [pivo] ++ quickSortLst (lstlstMenores lstLst pivo)
 
+frst :: (a, b, c) -> a -- As funções abaixo são para retirar elementos das tuplas, já que haskell só disponibiliza funções para manipular pares.
+frst (x, _, _) = x
+
+scnd :: (a, b, c) -> b
+scnd (_, x, _) = x
+
+thrd :: (a, b, c) -> c
+thrd (_, _, x) = x
+--------------------------------------------------------------
+
+-- Funções de Frequência --
+summaFreqs :: [(Float, Float, Int)] -> Int
+summaFreqs dataTable = sum $ map (thrd) dataTable
+
+--------------------------------------------------------------
+
+-- Calcular a média
+mediaPoint :: (Float, Float, Int) -> Float -- Retorna o ponto médio de uma classe
+mediaPoint dataClass = (frst dataClass + scnd dataClass)/2
+
+multPoint :: (Float, Float, Int) -> Float -- Retorna o ponto médio multiplicado pela frequência
+multPoint dataClass = fromIntegral (thrd dataClass) * mediaPoint dataClass
+
+summaMedia :: [(Float, Float, Int)] -> Float
+summaMedia dataTable = sum $ map (multPoint) dataTable
+
+mediaData :: [(Float, Float, Int)] -> Float
+mediaData dataTable = (summaMedia dataTable)/(fromIntegral $ summaFreqs dataTable)
+
 -- Encontrar a mediana
-divideData :: Int -> Int -- Divide o tamanho da lista por dois (função pra melhorar a leitura)
-divideData lengthLst = div lengthLst 2
+medianClass :: [(Float, Float, Int)] -> Int -> Int -> Int -> (Int, Float) -- Retorna o indice da classe em que a mediana está
+medianClass (x:dataTable) freq index medianPos 
+    | medianPos > freq+(thrd x) = medianClass dataTable (freq+(thrd x)) (index+1) medianPos
+    | otherwise = (index, fromIntegral freq)
 
-medianType :: [Float] -> Bool -- Verifica qual o caso de mediana(par/true ou impar/false)
-medianType dataLst = (mod (length dataLst) 2) == 0
+medianCalc :: [(Float, Float, Int)] -> Int -> Float
+medianCalc dataTable totalFreq = infLim+(((fromIntegral $ halfFreq) - acumulFreq)/freqClass)*amplit
+    where 
+        medianReturn = medianClass dataTable 0 0 halfFreq
+        medClass = dataTable !! (fst medianReturn) -- pegando o item da lista de tuplas através do indice retornado em "medianReturn"
+        -- Como a fórmula da mediana em dados agrupados é um pouco confusa escrita em linha de código, vou deixar
+        infLim = frst medClass -- limite inferior da classe
+        freqClass = fromIntegral $ thrd medClass -- frequência da classe
+        halfFreq = div totalFreq 2 -- metade da frequência
+        acumulFreq = snd medianReturn -- soma das frequências anteriores às da classe
+        amplit = (scnd medClass) - (frst medClass) -- Amplitude da classe
 
-medianPair :: [Float] -> Float -- Retorna a média dos dois números centrais da lista(mediana de num pares)
-medianPair dataLst = (dataLst !! divideData(length dataLst - 1) + dataLst !! divideData(length dataLst))/2
-
-medianOdd :: [Float] -> Float -- Retorna o número do centro da lista(mediana)
-medianOdd dataLst = dataLst !! (divideData $ length dataLst)
-
-medianData :: [Float] -> Float -- Retorna a mediana de uma lista de numeros
-medianData dataLst = 
-    if medianType dataLst then medianPair dataLst 
-    else medianOdd dataLst
+medianData :: [(Float, Float, Int)] -> Float -- Retorna a mediana de uma lista de numeros
+medianData dataTable = medianCalc dataTable $ summaFreqs dataTable 
 
 -- Encontrar a moda
 agrupaRepetidos :: [Float] -> [[Float]] -- Cria uma lista de listas de números repetidos dentro dos dados sendo analisados. A lista está ordenada de maneira crescente
@@ -83,20 +116,8 @@ modaData dataLst = buscaModa (ordenaRepetidos dataLst)
 
 -- Calcular o coeficiente de variação
 
--- Imprimir a tabela
-
--- Montar a tabela
-
-frst :: (a, b, c) -> a -- As funções abaixo são para retirar elementos das tuplas, já que haskell só disponibiliza funções para manipular pares.
-frst (x, _, _) = x
-
-scnd :: (a, b, c) -> b
-scnd (_, x, _) = x
-
-thrd :: (a, b, c) -> c
-thrd (_, _, x) = x
-
-contaFreqs :: Int -> Float -> Float -> [Float] -> Int
+-- Criação da Tabela de Classes --
+contaFreqs :: Int -> Float -> Float -> [Float] -> Int -- Conta as frequências com que cada classe aparece no conjunto de dados
 contaFreqs contador lowerLim upperLim (x:dataLst)
     | x < upperLim && x >= lowerLim = if null dataLst 
         then contador+1 
@@ -125,15 +146,14 @@ criaClasses lstClass numClass intervalo dataLst
     | length lstClass >= numClass = lstClass
     | otherwise = criaClasses (insertClass lstClass (fromIntegral intervalo) dataLst) numClass intervalo dataLst
 
-montaTabela :: [Float] -> Int -> [(Float, Float, Int)]
+montaTabela :: [Float] -> Int -> [(Float, Float, Int)] -- Retorna uma lista de tuplas com o limite inferior e limite superior das classes, assim como a frequência com que elementos dessa classe aparecem
 montaTabela dataLst numClass =
     let intervalo = intervaloClasses (amplitudeData dataLst) numClass
         lowerLim = head dataLst
         upperLim = head dataLst+(fromIntegral intervalo)
     in criaClasses [(lowerLim, upperLim, (contaFreqs 0 lowerLim upperLim dataLst))] numClass intervalo dataLst
 
-
 imprimeTabela :: [Float] -> [(Float, Float, Int)]
 imprimeTabela dataLst = montaTabela (quickSort dataLst) (numClasses $ length dataLst)
-
+--------------------------------------------------------------
 -- Função Main
