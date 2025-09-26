2767444085b513217326e4a4f667937efa40f2d7
rnnr11 <joaopedroquadrosmartins@gmail.com>
2025-09-26 11:58:47 -0300

    Início da implementação de banco de dados SQLite e testes de estudo.

---
+++ Scotty/src/testes/Teste.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
+
+lst :: [Float]
+lst = [31, 41, 47, 53, 57, 61, 65, 67, 73, 35, 41, 48, 54, 59, 64, 66, 68, 73, 35, 42, 50, 55, 60, 65, 66, 69, 74] -- Essa constante é pra verificar se os cálculos estão sendo operados, seja corretamente ou não
+
+insertLst :: [Float] -> Connection -> IO ()
+insertLst [] conn = do close conn
+insertLst (x:lst) conn = do
+    execute conn "INSERT INTO dataset (grupo, numero) VALUES (?,?)" ("Resultados de Teste de QI" :: String, x :: Float)
+    insertLst lst conn
+
+main :: IO ()
+main = do
+    conn <- open "dataset.db"
+    insertLst lst conn
